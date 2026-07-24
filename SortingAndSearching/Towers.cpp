a1afd7b7fa14bfc9ae4024fca3f48ed357299c2c
Angelo <julio.dasilva.telles@gmail.com>
2026-07-24 10:12:13 -0300

    Towers: Resolvido (Greddy, usa multiset)

---
+++ SortingAndSearching/Towers.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define sz(v) (int)v.size()
+#define PB push_back
+#define PP pop_back
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef priority_queue <ii, vector<ii>, greater<ii>> pq_min;
+
+multiset<int> blocks;
+
+signed main(){
+    int n, val, res = 0;;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> val;
+        auto it =  blocks.upper_bound(val);
+        if (it == blocks.end()) res++;
+        else blocks.erase(it);
+        blocks.insert(val);
+    }
+    cout << res << '\n';
+}   
